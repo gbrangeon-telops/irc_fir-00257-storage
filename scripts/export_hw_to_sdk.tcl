64030b2c39b45bb70b483c6ec214f460d9aef72d
odionne-telpos <olivier.dionne@telops.com>
2015-05-25 13:22:22 +0000

    ajout du fichier bmm dans l'export à SDK
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@15802 3e98269d-ff51-e449-abca-491a6567d830

--- scripts/export_hw_to_sdk.tcl
+++ scripts/export_hw_to_sdk.tcl
@@ -2,7 +2,8 @@ set root_dir "d:/Telops/fir-00257-Storage"
 set proj_name "fir_00257_Storage"
 set runs_dir "$root_dir/xilinx/$proj_name.runs"
 set top_level "fir_257_top"
+set top_level_bd "fir_257_top_bd"
 
 write_hwdef -force -file $runs_dir/synth_1/$top_level.hwdef
-write_sysdef -force -hwdef $runs_dir/synth_1/$top_level.hwdef -bitfile $runs_dir/impl_1/$top_level.bit -file $runs_dir/impl_1/$top_level.sysdef
+write_sysdef -force -hwdef $runs_dir/synth_1/$top_level.hwdef -bitfile $runs_dir/impl_1/$top_level.bit -meminfo $runs_dir/impl_1/$top_level_bd.bmm -file $runs_dir/impl_1/$top_level.sysdef
 file copy -force $runs_dir/impl_1/$top_level.sysdef $root_dir/sdk/hw/$top_level.hdf
