32ae8000ba6f0b20301d558916ae1467d209dfbb
odionne-telpos <olivier.dionne@telops.com>
2015-05-27 21:21:19 +0000

    RELEASE 0.1 (minimum pour tests EMI)
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@15827 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs.pl
+++ bin/svnrevs.pl
@@ -1,9 +1,9 @@
 our $rel_storage_hw_rev = 15824;
 our $rel_storage_hw_modified = 0;
-our $rel_storage_sw_rev = 15751;
-our $rel_storage_sw_modified = 1;
+our $rel_storage_sw_rev = 15824;
+our $rel_storage_sw_modified = 0;
 our $rel_storage_boot_rev = 0;
 our $rel_storage_boot_modified = 0;
 our $rel_storage_common_rev = 15751;
-our $rel_storage_common_modified = 1;
+our $rel_storage_common_modified = 0;
 1;
