9cba7c423c5a6778d31e14e58361acf0383907bb
odionne-telpos <olivier.dionne@telops.com>
2015-06-19 13:29:01 +0000

    timeout du wait mem ready augmenté de 5 à 10s
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@16073 3e98269d-ff51-e449-abca-491a6567d830

--- src/Buffering/Drivers/BufferManager.h
+++ src/Buffering/Drivers/BufferManager.h
@@ -27,7 +27,7 @@
 /************************** Constant Definitions ****************************/
 #define BUF_MAX_SEQUENCE 		255
 #define MEM_ADDR_GPIO_CH_ID     1
-#define MEM_READY_TIMEOUT_IN_S  5
+#define MEM_READY_TIMEOUT_IN_S  10
 
 /**
  * Memory address map.
