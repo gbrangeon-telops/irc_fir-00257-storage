be1009dcb2a9acddc86d8c16bf3c5d7accbb6971
dalain-telpos <denis.alain@telops.com>
2015-06-19 15:30:54 +0000

    XML 10.5.1
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@16079 3e98269d-ff51-e449-abca-491a6567d830

--- src/sw/GC_Registers.c
+++ src/sw/GC_Registers.c
@@ -19,7 +19,7 @@
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam library.
-// Generated from XML camera definition file version 10.5.0
+// Generated from XML camera definition file version 10.5.1
 // using generateGenICamCLib.m Matlab script.
 
 // GenICam global variables definition
--- src/sw/GC_Registers.h
+++ src/sw/GC_Registers.h
@@ -21,7 +21,7 @@
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam library.
-// Generated from XML camera definition file version 10.5.0
+// Generated from XML camera definition file version 10.5.1
 // using generateGenICamCLib.m Matlab script.
 
 // Registers data structure and data type
