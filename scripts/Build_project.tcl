bb32fa638bbbb0e1d46a695cd83884a333f71c87
odionne-telpos <olivier.dionne@telops.com>
2015-07-09 17:29:44 +0000

    script Build_project pour storage qui utilise script export_hw_to_sdk
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@16326 3e98269d-ff51-e449-abca-491a6567d830

--- scripts/Build_project.tcl
+++ scripts/Build_project.tcl
@@ -1,8 +1,8 @@
-set base_dir "d:/Telops/fir-00251-Proc"
+set base_dir "d:/Telops/fir-00257-Storage"
+set proj_name "fir_00257_Storage"
 
-
-#Build project PelicanD
-source $base_dir/scripts/pelicanD_project.tcl
+#Build project
+source $base_dir/scripts/gen_storage_project.tcl
 #generate bitstream
 launch_runs impl_1 -to_step write_bitstream
 #wait for run end
@@ -10,39 +10,8 @@ wait_on_run impl_1
 #open implemented design
 open_run impl_1
 #Open Block diagram
-open_bd_design {d:/Telops/fir-00251-Proc/xilinx/PelicanD/fir_00251_proc_pelicanD.srcs/sources_1/bd/core/core.bd}
+open_bd_design {$base_dir/xilinx/$proj_name.srcs/sources_1/bd/core/core.bd}
 #Export hardware for sdk
-export_hardware [get_files d:/Telops/fir-00251-Proc/xilinx/PelicanD/fir_00251_proc_pelicanD.srcs/sources_1/bd/core/core.bd] [get_runs impl_1] -bitstream -dir D:/Telops/FIR-00251-Proc/sdk/fir_00251_proc_pelicanD
-# End of this build
-close_project 
-
-
-#Build project ScorpiolwD
-source $base_dir/scripts/scorpiolwD_project.tcl
-#generate bitstream
-launch_runs impl_1 -to_step write_bitstream
-#wait for run end
-wait_on_run impl_1
-#open implemented design
-open_run impl_1
-#Open Block diagram
-open_bd_design {d:/Telops/fir-00251-Proc/xilinx/scorpiolwD/fir_00251_proc_scorpiolwD.srcs/sources_1/bd/core/core.bd}
-#Export hardware for sdk
-export_hardware [get_files d:/Telops/fir-00251-Proc/xilinx/scorpiolwD/fir_00251_proc_scorpiolwD.srcs/sources_1/bd/core/core.bd] [get_runs impl_1] -bitstream -dir D:/Telops/FIR-00251-Proc/sdk/fir_00251_proc_scorpiolwD
-# End of this build
-close_project
-
-#Build project MarsVlwD
-source $base_dir/scripts/marsD_project.tcl
-#generate bitstream
-launch_runs impl_1 -to_step write_bitstream
-#wait for run end
-wait_on_run impl_1
-#open implemented design
-open_run impl_1
-#Open Block diagram
-open_bd_design {d:/Telops/fir-00251-Proc/xilinx/marsD/fir_00251_proc_marsD.srcs/sources_1/bd/core/core.bd}
-#Export hardware for sdk
-export_hardware [get_files d:/Telops/fir-00251-Proc/xilinx/marsD/fir_00251_proc_marsD.srcs/sources_1/bd/core/core.bd] [get_runs impl_1] -bitstream -dir D:/Telops/FIR-00251-Proc/sdk/fir_00251_proc_marsD
+source $base_dir/scripts/export_hw_to_sdk.tcl
 # End of this build
 close_project
