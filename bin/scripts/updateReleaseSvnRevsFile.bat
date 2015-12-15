e80019313c0a19921201b1aab75c7353eba718c2
dalain-telpos <denis.alain@telops.com>
2015-12-15 16:49:55 +0000

    Release 1.3.x.2
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@17692 3e98269d-ff51-e449-abca-491a6567d830

--- bin/scripts/updateReleaseSvnRevsFile.bat
+++ bin/scripts/updateReleaseSvnRevsFile.bat
@@ -4,18 +4,14 @@ rem Clean up
 del %revFile%
 
 echo our $rel_storage_hw_rev = $WCREV$;> %revFile%
-echo our $rel_storage_hw_modified = $WCMODS?1:0$;>> %revFile%
 %svn_subwcrev% %bitFile% %revFile% %revFile%
 
 echo our $rel_storage_sw_rev = $WCREV$;>> %revFile%
-echo our $rel_storage_sw_modified = $WCMODS?1:0$;>> %revFile%
 %svn_subwcrev% %elfFile% %revFile% %revFile%
 
 echo our $rel_storage_boot_rev = 0;>> %revFile%
-echo our $rel_storage_boot_modified = 0;>> %revFile%
 
 echo our $rel_storage_common_rev = $WCREV$;>> %revFile%
-echo our $rel_storage_common_modified = $WCMODS?1:0$;>> %revFile%
 %svn_subwcrev% %commonDir% %revFile% %revFile%
 
 echo 1;>> %revFile%
--- bin/svnrevs.pl
+++ bin/svnrevs.pl
@@ -1,9 +1,5 @@
 our $rel_storage_hw_rev = 17065;
-our $rel_storage_hw_modified = 0;
-our $rel_storage_sw_rev = 17636;
-our $rel_storage_sw_modified = 0;
+our $rel_storage_sw_rev = 17690;
 our $rel_storage_boot_rev = 0;
-our $rel_storage_boot_modified = 0;
-our $rel_storage_common_rev = 17630;
-our $rel_storage_common_modified = 0;
+our $rel_storage_common_rev = 17680;
 1;
