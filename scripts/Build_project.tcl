c0ea5d66602cf779e894655629163d2d5965ccc5
odionne-telpos <olivier.dionne@telops.com>
2016-02-12 19:03:21 +0000

    ajout des reports; delete des debug_cores
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@18006 3e98269d-ff51-e449-abca-491a6567d830

--- scripts/Build_project.tcl
+++ scripts/Build_project.tcl
@@ -9,8 +9,13 @@ launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 #open implemented design
 open_run impl_1
+#Save reports
+report_timing_summary -file $base_dir/reports/${proj_name}_timing_summary_routed.rpt
+report_clock_utilization -file $base_dir/reports/${proj_name}_clock_utilization_placed.rpt
+report_utilization -file $base_dir/reports/${proj_name}_utilization_placed.rpt
+report_utilization -file $base_dir/reports/${proj_name}_utilization_placed_hier.rpt -hierarchical -hierarchical_depth 5
 #Open Block diagram
-#open_bd_design {$base_dir/xilinx/$proj_name.srcs/sources_1/bd/core/core.bd}
+#open_bd_design $base_dir/xilinx/$proj_name.srcs/sources_1/bd/core/core.bd
 #Export hardware for sdk
 source $base_dir/scripts/export_hw_to_sdk.tcl
 # End of this build
