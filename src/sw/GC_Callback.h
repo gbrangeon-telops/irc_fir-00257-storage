69b42b24e643a514e3169e34b7cf42648e78b168
dalain-telpos <denis.alain@telops.com>
2016-03-08 14:41:13 +0000

    XML 11.3.1
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@18183 3e98269d-ff51-e449-abca-491a6567d830

--- src/sw/GC_Callback.h
+++ src/sw/GC_Callback.h
@@ -20,7 +20,7 @@
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam registers callback functions definition.
-// Generated from XML camera definition file version 11.3.0
+// Generated from XML camera definition file version 11.3.1
 // using updateGenICamCallback.m Matlab script.
 
 void GC_Callback_Init();
--- src/sw/GC_Registers.c
+++ src/sw/GC_Registers.c
@@ -22,7 +22,7 @@
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam library.
-// Generated from XML camera definition file version 11.3.0
+// Generated from XML camera definition file version 11.3.1
 // using generateGenICamCLib.m Matlab script.
 
 // GenICam global variables definition
