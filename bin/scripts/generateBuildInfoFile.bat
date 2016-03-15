4b2c60bc44870cdac8566a182ad2b5b326c43ea5
dalain-telpos <denis.alain@telops.com>
2016-03-15 19:49:21 +0000

    Check for hardware definition file mismatch.
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@18251 3e98269d-ff51-e449-abca-491a6567d830

--- bin/scripts/generateBuildInfoFile.bat
+++ bin/scripts/generateBuildInfoFile.bat
@@ -7,24 +7,38 @@ echo #ifndef BUILDINFO_H> %buildInfoFile%
 echo #define BUILDINFO_H>> %buildInfoFile%
 echo.>> %buildInfoFile%
 
-% rem Get hardware revision
+rem Get hardware revision
 echo #define SVN_HARDWARE_REV   $WCMODS?-:$$WCREV$>> %buildInfoFile%
 %svn_subwcrev% %bitFile% %buildInfoFile% %buildInfoFile%
 
-% rem Get software revision
+rem Get software revision
 echo #define SVN_SOFTWARE_REV   $WCMODS?-:$$WCREV$>> %buildInfoFile%
 %svn_subwcrev% %elfFile% %buildInfoFile% %buildInfoFile%
 
-% rem Get common directory revision
+rem Get common directory revision
 echo #define SVN_COMMON_REV     $WCMODS?-:$$WCREV$>> %buildInfoFile%
 %svn_subwcrev% %commonDir% %buildInfoFile% %buildInfoFile%
 
-% rem Check for uncommitted changes
+rem Check for uncommitted changes
 echo.>> %buildInfoFile%
 echo #define SVN_UNCOMMITTED_CHANGES  ((SVN_HARDWARE_REV ^< 0) ^|^| (SVN_SOFTWARE_REV ^< 0) ^|^| (SVN_COMMON_REV ^< 0))>> %buildInfoFile%
 echo.>> %buildInfoFile%
 echo #if SVN_UNCOMMITTED_CHANGES>> %buildInfoFile%
 echo #warning Uncommitted changes detected.>> %buildInfoFile%
 echo #endif>> %buildInfoFile%
+
+rem Check for hardware definition file mismatch
+set hwFile1=%sdkDir%\hw_platform_0\system.hdf
+set hwFile2=%sdkDir%\hw\fir_257_top.hdf
+%x_xilperl% %scriptsDir%\compareFiles.pl -f1 %hwFile1% -f2 %hwFile2%
+set hardwareMismatch=%errorlevel%
+
 echo.>> %buildInfoFile%
+echo #define HARDWARE_MISMATCH (%hardwareMismatch%)>> %buildInfoFile%
+echo.>> %buildInfoFile%
+echo #if HARDWARE_MISMATCH>> %buildInfoFile%
+echo #error %hwFile1% does not match %hwFile2%.>> %buildInfoFile%
 echo #endif>> %buildInfoFile%
+
+echo.>> %buildInfoFile%
+echo #endif // BUILDINFO_H>> %buildInfoFile%
