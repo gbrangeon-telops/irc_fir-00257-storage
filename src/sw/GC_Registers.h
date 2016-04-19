959d23f561c5f5c12d7f0aff6a68b9a767aa84bb
dalain-telpos <denis.alain@telops.com>
2016-04-19 00:25:34 +0000

    - ActualizationPOSIXTime header field is initialized using 0 instead of gActualisationPosixTime (#7966). - XML 11.4.0
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@18571 3e98269d-ff51-e449-abca-491a6567d830

--- src/sw/GC_Registers.h
+++ src/sw/GC_Registers.h
@@ -23,10 +23,10 @@
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam library.
-// Generated from XML camera definition file version 11.3.1
+// Generated from XML camera definition file version 11.4.0
 // using generateGenICamCLib.m Matlab script.
 
-#if ((GC_XMLMAJORVERSION != 11) || (GC_XMLMINORVERSION != 3) || (GC_XMLSUBMINORVERSION != 1))
+#if ((GC_XMLMAJORVERSION != 11) || (GC_XMLMINORVERSION != 4) || (GC_XMLSUBMINORVERSION != 0))
 #error "XML version mismatch."
 #endif
 
