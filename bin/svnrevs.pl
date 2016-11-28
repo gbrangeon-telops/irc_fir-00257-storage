5255345c60c39fc0ec112e794d19a0442b9ec36d
dalain-telpos <denis.alain@telops.com>
2016-11-28 21:00:26 +0000

    Release 2.0.x.4 (Without change made in revision 19662)
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@19663 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs.pl
+++ bin/svnrevs.pl
@@ -1,5 +1,5 @@
 our $rel_storage_hw_rev = 18307;
-our $rel_storage_sw_rev = 19469;
+our $rel_storage_sw_rev = 19661;
 our $rel_storage_boot_rev = 0;
-our $rel_storage_common_rev = 19463;
+our $rel_storage_common_rev = 19547;
 1;
