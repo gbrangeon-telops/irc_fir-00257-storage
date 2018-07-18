9da852407bd3b2388aecccdaaabe3ab69b9c4754
odionne-telpos <olivier.dionne@telops.com>
2018-07-18 17:52:44 +0000

    Release 2.5.x.0
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@22030 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs.pl
+++ bin/svnrevs.pl
@@ -1,5 +1,5 @@
 our $rel_storage_hw_rev = 20802;
-our $rel_storage_sw_rev = 21545;
+our $rel_storage_sw_rev = 22029;
 our $rel_storage_boot_rev = 0;
-our $rel_storage_common_rev = 21541;
+our $rel_storage_common_rev = 21999;
 1;
