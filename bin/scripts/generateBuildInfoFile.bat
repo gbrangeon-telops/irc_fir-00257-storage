38378ecf5f7d44f032ef38e40aef50ef81515923
elarouche-telpos <eric.larouche@telops.com>
2018-11-19 19:21:59 +0000

    ajustement du BuildInfo
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@22488 3e98269d-ff51-e449-abca-491a6567d830

--- bin/scripts/generateBuildInfoFile.bat
+++ bin/scripts/generateBuildInfoFile.bat
@@ -1,14 +1,21 @@
 @echo off
-call D:\Telops\FIR-00257-Storage\bin\scripts\setEnvironment.bat
+
+rem
+rem 16 GB
+rem
+call D:\Telops\FIR-00257-Storage\bin\scripts\setEnvironment.bat 16
 
 rem Configuration variable
-set conf=%1
+set conf=16
 
 rem Clean up
 del %buildInfoFile%
 
 echo #ifndef BUILDINFO_H> %buildInfoFile%
 echo #define BUILDINFO_H>> %buildInfoFile%
+echo.>> %buildInfoFile%
+
+echo #if MEMCONF == 16>> %buildInfoFile%
 echo.>> %buildInfoFile%
 
 rem Get hardware revision
@@ -45,4 +52,53 @@ echo #error %hwFile1% does not match %hwFile2%.>> %buildInfoFile%
 echo #endif>> %buildInfoFile%
 
 echo.>> %buildInfoFile%
+
+rem
+rem 32 GB
+rem
+call D:\Telops\FIR-00257-Storage\bin\scripts\setEnvironment.bat 32
+
+rem Configuration variable
+set conf=32
+
+echo #elif MEMCONF == 32>> %buildInfoFile%
+echo.>> %buildInfoFile%
+
+rem Get hardware revision
+echo #define SVN_HARDWARE_REV   $WCMODS?-:$$WCREV$>> %buildInfoFile%
+%svn_subwcrev% %bitFile% %buildInfoFile% %buildInfoFile%
+
+rem Get software revision
+echo #define SVN_SOFTWARE_REV   $WCMODS?-:$$WCREV$>> %buildInfoFile%
+%svn_subwcrev% %elfFile% %buildInfoFile% %buildInfoFile%
+
+rem Get common directory revision
+echo #define SVN_COMMON_REV     $WCMODS?-:$$WCREV$>> %buildInfoFile%
+%svn_subwcrev% %commonDir% %buildInfoFile% %buildInfoFile%
+
+rem Check for uncommitted changes
+echo.>> %buildInfoFile%
+echo #define SVN_UNCOMMITTED_CHANGES  ((SVN_HARDWARE_REV ^< 0) ^|^| (SVN_SOFTWARE_REV ^< 0) ^|^| (SVN_COMMON_REV ^< 0))>> %buildInfoFile%
+echo.>> %buildInfoFile%
+echo #if SVN_UNCOMMITTED_CHANGES>> %buildInfoFile%
+echo #warning Uncommitted changes detected.>> %buildInfoFile%
+echo #endif>> %buildInfoFile%
+
+rem Check for hardware definition file mismatch
+set hwFile1=%sdkDir%\hw_platform_%conf%\system.hdf
+set hwFile2=%sdkDir%\hw_%conf%\fir_257_top.hdf
+%x_xilperl% %scriptsDir%\compareFiles.pl -f1 %hwFile1% -f2 %hwFile2%
+set hardwareMismatch=%errorlevel%
+
+echo.>> %buildInfoFile%
+echo #define HARDWARE_MISMATCH (%hardwareMismatch%)>> %buildInfoFile%
+echo.>> %buildInfoFile%
+echo #if HARDWARE_MISMATCH>> %buildInfoFile%
+echo #error %hwFile1% does not match %hwFile2%.>> %buildInfoFile%
+echo #endif>> %buildInfoFile%
+echo.>> %buildInfoFile%
+
+echo #endif >> %buildInfoFile%
+echo.>> %buildInfoFile%
+
 echo #endif // BUILDINFO_H>> %buildInfoFile%
--- src/BuildInfo/BuildInfo.h
+++ src/BuildInfo/BuildInfo.h
@@ -1,8 +1,10 @@
 #ifndef BUILDINFO_H
 #define BUILDINFO_H
 
-#define SVN_HARDWARE_REV   21999
-#define SVN_SOFTWARE_REV   21999
+#if MEMCONF == 16
+
+#define SVN_HARDWARE_REV   22474
+#define SVN_SOFTWARE_REV   22474
 #define SVN_COMMON_REV     21999
 
 #define SVN_UNCOMMITTED_CHANGES  ((SVN_HARDWARE_REV < 0) || (SVN_SOFTWARE_REV < 0) || (SVN_COMMON_REV < 0))
@@ -16,5 +18,25 @@
 #if HARDWARE_MISMATCH
 #error D:\Telops\FIR-00257-Storage\sdk\hw_platform_16\system.hdf does not match D:\Telops\FIR-00257-Storage\sdk\hw_16\fir_257_top.hdf.
 #endif
+
+#elif MEMCONF == 32
+
+#define SVN_HARDWARE_REV   22474
+#define SVN_SOFTWARE_REV   22474
+#define SVN_COMMON_REV     21999
+
+#define SVN_UNCOMMITTED_CHANGES  ((SVN_HARDWARE_REV < 0) || (SVN_SOFTWARE_REV < 0) || (SVN_COMMON_REV < 0))
+
+#if SVN_UNCOMMITTED_CHANGES
+#warning Uncommitted changes detected.
+#endif
+
+#define HARDWARE_MISMATCH (0)
+
+#if HARDWARE_MISMATCH
+#error D:\Telops\FIR-00257-Storage\sdk\hw_platform_32\system.hdf does not match D:\Telops\FIR-00257-Storage\sdk\hw_32\fir_257_top.hdf.
+#endif
+
+#endif 
 
 #endif // BUILDINFO_H
