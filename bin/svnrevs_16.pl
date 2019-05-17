2979060592642f267dc2ba86682ed1b4dc5100a8
elarouche-telpos <eric.larouche@telops.com>
2019-05-17 13:51:01 +0000

    Release 3.0.x.0
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@23567 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs_16.pl
+++ bin/svnrevs_16.pl
@@ -1,5 +1,5 @@
-our $rel_storage_hw_rev1 = 22626;
-our $rel_storage_sw_rev1 = 22626;
+our $rel_storage_hw_rev1 = 23498;
+our $rel_storage_sw_rev1 = 23566;
 our $rel_storage_boot_rev1 = 0;
-our $rel_storage_common_rev1 = 22628;
+our $rel_storage_common_rev1 = 23551;
 1;
--- bin/svnrevs_32.pl
+++ bin/svnrevs_32.pl
@@ -1,5 +1,5 @@
-our $rel_storage_hw_rev2 = 22626;
-our $rel_storage_sw_rev2 = 22626;
+our $rel_storage_hw_rev2 = 23498;
+our $rel_storage_sw_rev2 = 23566;
 our $rel_storage_boot_rev2 = 0;
-our $rel_storage_common_rev2 = 22628;
+our $rel_storage_common_rev2 = 23551;
 1;
