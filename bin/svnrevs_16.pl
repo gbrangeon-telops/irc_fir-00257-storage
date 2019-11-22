2341f5cbbb4cb8811e7fb5a802b7a5aee6f6cd9f
pcouture-telpos <philippe.couture@telops.com>
2019-11-22 16:49:52 +0000

    Release 3.1.x.0
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@24515 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs_16.pl
+++ bin/svnrevs_16.pl
@@ -1,5 +1,5 @@
 our $rel_storage_hw_rev1 = 24031;
-our $rel_storage_sw_rev1 = 24451;
+our $rel_storage_sw_rev1 = 24514;
 our $rel_storage_boot_rev1 = 0;
-our $rel_storage_common_rev1 = 24443;
+our $rel_storage_common_rev1 = 24509;
 1;
--- bin/svnrevs_32.pl
+++ bin/svnrevs_32.pl
@@ -1,5 +1,5 @@
 our $rel_storage_hw_rev2 = 24031;
-our $rel_storage_sw_rev2 = 24451;
+our $rel_storage_sw_rev2 = 24514;
 our $rel_storage_boot_rev2 = 0;
-our $rel_storage_common_rev2 = 24443;
+our $rel_storage_common_rev2 = 24509;
 1;
