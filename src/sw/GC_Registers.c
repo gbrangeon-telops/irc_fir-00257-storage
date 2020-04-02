fdaf711390c2d6753aa023b5f7b87aad5d294b27
odionne-telpos <olivier.dionne@telops.com>
2020-04-02 20:32:19 +0000

    XML 12.9.0
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@25119 3e98269d-ff51-e449-abca-491a6567d830

--- src/sw/GC_Registers.c
+++ src/sw/GC_Registers.c
@@ -23,7 +23,7 @@
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam library.
-// Generated from XML camera definition file version 12.8.0
+// Generated from XML camera definition file version 12.9.0
 // using generateGenICamCLib.m Matlab script.
 
 // GenICam global variables definition
