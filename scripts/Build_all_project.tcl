cc03154401df5888316a00b6520821ad63c00643
pcouture-telpos <philippe.couture@telops.com>
2020-04-20 14:19:39 +0000

    Mise à jour script de build
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@25183 3e98269d-ff51-e449-abca-491a6567d830

--- scripts/Build_all_project.tcl
+++ scripts/Build_all_project.tcl
@@ -1,2 +1,4 @@
-source Build_project_16.tcl
-source Build_project_32.tcl
+set base_dir "d:/Telops/FIR-00257-Storage"
+
+source $base_dir/scripts/Build_project_16.tcl
+source $base_dir/scripts/Build_project_32.tcl
