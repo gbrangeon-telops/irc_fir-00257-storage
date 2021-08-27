c91f146a908c91b8ee14e6f259670e750fc23815
pcouture-telpos <philippe.couture@telops.com>
2021-08-27 18:59:56 +0000

    Release 5.1.x.1
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@26778 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs_16.pl
+++ bin/svnrevs_16.pl
@@ -1,5 +1,5 @@
 our $rel_storage_hw_rev1 = 25987;
-our $rel_storage_sw_rev1 = 26584;
+our $rel_storage_sw_rev1 = 26777;
 our $rel_storage_boot_rev1 = 0;
-our $rel_storage_common_rev1 = 26551;
+our $rel_storage_common_rev1 = 26738;
 1;
