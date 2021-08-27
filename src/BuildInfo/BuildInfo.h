653e22a3f8da3f01627666fd95fffc5dae2e8950
pcouture-telpos <philippe.couture@telops.com>
2021-08-27 18:58:06 +0000

    Pre-release 5.1.x.1
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@26777 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/BuildInfo.h
+++ src/BuildInfo/BuildInfo.h
@@ -4,7 +4,7 @@
 #if MEMCONF == 16
 
 #define SVN_HARDWARE_REV      25987
-#define SVN_SOFTWARE_REV      26585
+#define SVN_SOFTWARE_REV      26776
 #define SVN_BOOTLOADER_REV    0 
 #define SVN_COMMON_REV        26738
 
@@ -23,7 +23,7 @@
 #elif MEMCONF == 32
 
 #define SVN_HARDWARE_REV      25987
-#define SVN_SOFTWARE_REV      26585
+#define SVN_SOFTWARE_REV      26776
 #define SVN_BOOTLOADER_REV    0 
 #define SVN_COMMON_REV        26738
 
