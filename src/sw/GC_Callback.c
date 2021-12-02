d7a79be16c4e5218e77195a094777c90fc075005
odionne-telpos <olivier.dionne@telops.com>
2021-12-02 20:42:23 +0000

    XML 13.1.0
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@27001 3e98269d-ff51-e449-abca-491a6567d830

--- src/sw/GC_Callback.c
+++ src/sw/GC_Callback.c
@@ -34,7 +34,7 @@ extern t_bufferManager gBufManager;
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam registers callback functions definition.
-// Generated from XML camera definition file version 13.0.0
+// Generated from XML camera definition file version 13.1.0
 // using updateGenICamCallback.m Matlab script.
 
 /**
--- src/sw/GC_Callback.h
+++ src/sw/GC_Callback.h
@@ -20,7 +20,7 @@
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam registers callback functions definition.
-// Generated from XML camera definition file version 13.0.0
+// Generated from XML camera definition file version 13.1.0
 // using updateGenICamCallback.m Matlab script.
 
 void GC_Callback_Init();
