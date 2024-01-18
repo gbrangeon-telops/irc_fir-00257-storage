928006c36ebf5a986bec4393e7616dfcb30d22eb
elarouche-telpos <eric.larouche@telops.com>
2024-01-18 19:37:55 +0000

    revert du test temporaire de release
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@29790 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs_32.tcl
+++ bin/svnrevs_32.tcl
@@ -1,4 +1,4 @@
 set rel_storage_hw_rev2 29309
-set rel_storage_sw_rev2 29781
+set rel_storage_sw_rev2 29614
 set rel_storage_boot_rev2 0
-set rel_storage_common_rev2 29734
+set rel_storage_common_rev2 29542
