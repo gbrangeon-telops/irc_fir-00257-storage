e7d3b741e5e583315f11e6932467dccde5c8905e
elarouche-telpos <eric.larouche@telops.com>
2024-01-26 17:13:43 +0000

    "Pre-release 6.0.x.4"
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@29871 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/BuildInfo.h
+++ src/BuildInfo/BuildInfo.h
@@ -15,7 +15,7 @@
 #if MEMCONF == 16
  
 #define SVN_HARDWARE_REV 29309
-#define SVN_SOFTWARE_REV      29820
+#define SVN_SOFTWARE_REV      29821
  #define SVN_BOOTLOADER_REV    0 
  #define SVN_COMMON_REV      29734
  
@@ -28,7 +28,7 @@
 #elif MEMCONF == 32
  
 #define SVN_HARDWARE_REV 29309
-#define SVN_SOFTWARE_REV      29820
+#define SVN_SOFTWARE_REV      29821
  #define SVN_BOOTLOADER_REV    0 
  #define SVN_COMMON_REV      29734
  
