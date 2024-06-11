1d4b7af3e7d2702190cdd0d88f9c545f81cd78c2
odionne-telpos <olivier.dionne@telops.com>
2024-06-11 13:40:28 +0000

    merge de la branche 2024-04-17Release6.1.10.1
    git-svn-id: http://einstein/svn/firmware/FIR-00257-Storage/trunk@30417 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs_16.tcl
+++ bin/svnrevs_16.tcl
@@ -1,4 +1,4 @@
 set rel_storage_hw_rev1 29309
-set rel_storage_sw_rev1 29996
+set rel_storage_sw_rev1 30374
 set rel_storage_boot_rev1 0
 set rel_storage_common_rev1 29981
--- bin/svnrevs_32.tcl
+++ bin/svnrevs_32.tcl
@@ -1,4 +1,4 @@
 set rel_storage_hw_rev2 29309
-set rel_storage_sw_rev2 29996
+set rel_storage_sw_rev2 30374
 set rel_storage_boot_rev2 0
 set rel_storage_common_rev2 29981
